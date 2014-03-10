ac3dbcf34c2cbf25913c3a28053e176ac20ce9ad
Troy Cornwall <nztrooy@gmail.com>
2014-03-10 22:42:30 +1300

    fb comment styling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
+var url = require("url");
 
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
@@ -16,7 +17,7 @@ var mysql      = require('mysql');
 var FACEBOOK_APP_ID = "427162887386428";
 var FACEBOOK_APP_SECRET = "2c58b6b44fe3b970e09e1b8e0deb5716";
 
-var connection = mysql.createConnection({
+var sqldb = mysql.createConnection({
     host     : 'ep9gru174l.database.windows.net',
     user     : 'flamingo',
     password : 'IWishICouldRememberAllThis#69',
@@ -85,23 +86,27 @@ app.get('/', function(req, res){
 });
 
 app.get('/submit', ensureAuthenticated, function(req, res){
-    var result;
+    /*var result = "";
     //if(req.query.searchTerm){
-    connection.query('SELECT * FROM *', function(err, rows) {
-      if(err) {return console.log(err);}// connected! (unless `err` is set)
-      console.log(rows);
+		console.log(1);
+    sqldb.query('SELECT * FROM *', function(err, rows) {
+	    console.log('2a');
+      if(err) {console.log('err:\n'+err);}// connected! (unless `err` is set)
+      console.log('2b');
+	    console.log("rows\n" + rows);
+
       result= rows;
     });
-		var test = connection.query('SELECT * FROM *');
-		console.log(test);
+	console.log(1);
+		var test = sqldb.query('SELECT * FROM *');
+		console.log("test "+test+ "\n result" + result);
+	console.log();
+	var uni = url.parse(req.url).query['u'];
+	if(uni){console.log(uni);}
    // console.log(result);    //SQL TO GET ALL COURSES
        //RESULT TO JSON
-   // }
-    res.render('submitIndex', { user: req.user, title: 'Examinate - Submit', result: connection.query('SELECT * FROM *', function(err, rows) {
-	    if(err) {return console.log(err);}// connected! (unless `err` is set)
-	    console.log(rows);
-	    return rows;
-    }) });
+   // }*/
+    res.render('submitIndex', { user: req.user, title: 'Examinate - Submit'/*, result: test*/});
 });
 app.get('/check', ensureAuthenticated, function(req, res){
     res.render('checkIndex', { user: req.user, title: 'Examinate - Check' });
